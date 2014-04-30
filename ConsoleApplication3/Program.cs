059a33dfc70195fecd264138a40d23435b4ebd82
Sondosia <zombie_warrior@hotmail.com>
2014-04-30 13:41:30 +1200

    Steves stuff

---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    public static class StorageManagement
+    {
+        static void getLevelData()
+        {
+        }
+
+        static void getLevelList()
+        {
+        }
+        
+        static void getCustomSettings()
+        {
+        }
+        
+        static void getHighScores()
+        {        
+        }
+        
+        static void autoSaveProgress(){
+        {        
+        }
+        
+        static void savePlayProgress()
+        {
+        }
+        
+        static void loadPlayProgress()
+        {
+        }
+        
+        static void saveLevel()
+        {
+        }    
+        
+        static void publishLevel()
+        {
+        }
+    }
+}
---
+++ ConsoleApplication3/storageManagement.cs
@@ -0,0 +1,87 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    public static class StorageManagement
+    {
+        //Returns the level data to the gameplayer or level designer
+        static String getLevelData()
+        {
+            //ex var "GameBoard" data
+            //public String GameBoard = "233311244113441144413333"
+            //Replace with variable
+            return "233311244113441144413333"; 
+        }
+        
+        //Returns the start location of the minitor to the gameplayer or level designer
+        static int getStartMinitor()
+        {
+            return ; 
+        }
+        //sgfdgsds
+
+
+
+        //Returns the start of Theseus location to the gameplayer or level designer
+        static int getStartTheseus()
+        {
+            return ; 
+        }
+        
+        //Returns the exit location to the gameplayer or level designer
+        static int getLevelExit()
+        {
+            return ; 
+        }
+
+        //Returns the level list to the gameplayer 
+        static string getLevelList()
+        {
+            return ;
+        }
+        
+        //Returns the customer settings (Sound, walls, icons) to the gameplayer 
+        /*
+        static ?void? getCustomSettings()
+        {
+
+        }
+        
+
+        //Returns the level highscores to the gameplayer 
+        static string getHighScores()
+        {        
+
+        }
+        
+        //Auto Saves the player progress to storage
+        static ?void? autoSaveProgress(){
+        {        
+        }
+        
+        //Saves the player progress to storage
+        static void savePlayerProgress()
+        {
+        }
+        
+        //Returns the player progress to the gameplayer
+        static void loadPlayerProgress()
+        {
+        }
+        
+        //Saves a level to storage
+        static void saveLevel()
+        {
+        }    
+        
+        //Saves a finished level to storage
+        static void publishLevel()
+        {
+        }
+    }
+}
+*/
